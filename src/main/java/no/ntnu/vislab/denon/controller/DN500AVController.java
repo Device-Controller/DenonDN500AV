106603fa4ede5ca87f53b5181296f6bbb545f960
ThomasSTodal <ThomaTod@stud.ntnu.no>
2018-05-02 12:54:35 +0200

    git commit

--- src/main/java/no/ntnu/vislab/denon/controller/DN500AVController.java
+++ src/main/java/no/ntnu/vislab/denon/controller/DN500AVController.java
@@ -1,5 +1,6 @@
 package no.ntnu.vislab.denon.controller;
 
+import no.ntnu.vislab.denon.driver.DN500AVDevice;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -7,9 +8,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/denondn500av")
-public class DN500AVController {
+@RequestMapping("/DenonDN500AV")
+public class DN500AVController  extends DeviceManager {
 
+    @RequestMapping("/powerOn")
+    public ResponseEntity<Integer> powerOn(@RequestParam("id") int id) {
+        DN500AVDevice device = get
+        return new ResponseEntity<>();
+    }
 
     @RequestMapping("/echo")
     public ResponseEntity<String> yolo(@RequestParam ("echo") String echo){
