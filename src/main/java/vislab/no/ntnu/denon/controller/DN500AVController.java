fa3f7bce90bd1ddbee76ef919a4cca2d575878bf
erikhny <erikhny@gmail.com>
2018-05-09 12:11:08 +0200

    Added webpage for dn500av controller with js and css

--- src/main/java/vislab/no/ntnu/denon/controller/DN500AVController.java
+++ src/main/java/vislab/no/ntnu/denon/controller/DN500AVController.java
@@ -78,7 +78,7 @@ public class DN500AVController  extends DeviceManager {
     @Override
     public String getDevicePage(int id){
         if(getSoundSystem(id) != null){
-            return "forward:/denon/denon.html";
+            return "forward:/dn500av/dn500av.html";
         }
         return super.getDevicePage(id);
     }
