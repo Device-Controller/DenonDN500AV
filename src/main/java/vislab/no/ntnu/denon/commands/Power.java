8507ad121909c4c2492b5e70255c676b0ec67d57
Kristoffer <Kristoffer_rogne@hotmail.com>
2018-05-09 11:52:05 +0200

    Removed physical library, use install on library project to build

--- src/main/java/vislab/no/ntnu/denon/commands/Power.java
+++ src/main/java/vislab/no/ntnu/denon/commands/Power.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Power extends DN500AVCommand {
-    private static final String POWER = "PW";
+    public static final String POWER = "PW";
     public static final String ON = "ON";
     public static final String OFF = "STANDBY";
     private List<String> powerStates = new ArrayList<>();
